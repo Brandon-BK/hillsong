91dbf59c49fd4e3c1ebfefca6efd9ee3dc6e48e2
brandon <kisibubrandon5@gmail.com>
2022-03-08 13:13:19 +0200

    template

---
+++ components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+const NavBar = () => {
+  return (
+    <div style={{width:"100%",height:359,backgroundImage:`url(bg.jpg)`,backgroundPosition:'center',backgroundRepeat:'no-repeat',backgroundSize:'cover',objectFit:'cover',paddingTop:10}}>
+
+        <nav style={{width:'100%',height:80,display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+            <div style={{width:200,height:80,display:'flex',alignItems:'center'}}>
+                <img src="./logo.svg" height="60px" width="180px" />
+            </div>
+
+
+
+            <div style={{width:600,height:80,display:'flex'}}>
+                <p style={{flex:1,display:'flex',alignItems:'center',justifyContent:'center',fontSize:14}}><a className={styles.links} style={{cursor:'pointer'}}>BOOKS</a></p>
+                <p style={{flex:1,display:'flex',alignItems:'center',justifyContent:'center',fontSize:14}}><a className={styles.links} style={{cursor:'pointer'}}>MUSIC</a></p>
+                <p style={{flex:1,display:'flex',alignItems:'center',justifyContent:'center',fontSize:14}}><a className={styles.links} style={{cursor:'pointer'}}>TEACHING</a></p>
+                <p style={{flex:1,display:'flex',alignItems:'center',justifyContent:'center',fontSize:14}}><a className={styles.links} style={{cursor:'pointer'}}>CARRICULUM</a></p>
+                <p style={{flex:1,display:'flex',alignItems:'center',justifyContent:'center',fontSize:14}}><a className={styles.links} style={{cursor:'pointer'}}>APPAREL</a></p>
+                <p style={{flex:1,display:'flex',alignItems:'center',justifyContent:'center',fontSize:14}}><a className={styles.links} style={{cursor:'pointer'}}>POSTERS</a></p>
+            </div>
+
+
+            
+            <div style={{width:500,height:80,display:'flex',border:"1px solid green",fontSize:13,alignItems:'center',paddingLeft:40}}>
+                <div>
+                    <img src="./flag.png" height="17px" width="21px" />
+                </div>
+                <p style={{marginLeft:5,marginRight:20}}>USD - US Dollar</p>
+                <KeyboardArrowDownIcon />
+            </div>
+            
+        </nav>
+        <div style={{fontSize:12,paddingLeft:30,marginTop:10}}>Home/Book</div>
+        
+    </div>
+  )
+}
+
+export default NavBar
